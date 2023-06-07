32465e6af6d6b9542f35f5cdd0bdebaf10d84d4d
jenishWFT <jenish@waterflowtechnology.net>
2023-06-07 13:22:53 +0545

    feat: changed entities

--- Boss.Gateway.Domain/Entities/Bills/SellBills/test.cs
+++ Boss.Gateway.Domain/Entities/Bills/SellBills/test.cs
@@ -2,35 +2,8 @@ namespace Boss.Gateway.Domain.Entities;
 
 public class SellBilles
 {
-    public SellBilles()
-    {
-        this.Id = Guid.NewGuid();
-    }
 
-    public Guid Id { get; set; }
+    public string Task { get; set; } = "";
 
-    public string ClientCode { get; set; } = "";
-
-    public string ClientName { get; set; } = "";
-
-    public string BillNumber { get; set; } = "";
-
-    public string BillDate { get; set; } = "";
-
-    public decimal BrokerCommission { get; set; } = 0;
-
-    public decimal NepseCommission { get; set; } = 0;
-
-    public decimal SeboCommission { get; set; } = 0;
-
-    public decimal SeboRegulatoryFee { get; set; } = 0;
-
-    public string ClearanceDate { get; set; } = "";
-
-    public decimal DpAmount { get; set; } = 0;
-
-    public Guid FloorsheetId { get; set; }
-
-    public decimal NameTransferAmount { get; set; } = 0;
 
 }
